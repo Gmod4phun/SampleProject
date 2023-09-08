808462247f2f81501a2cbc2891fa3532a54403c5
Patrik Šimanský <Gmod4phun@gmail.com>
2023-09-08 15:52:14 +0200

    Updated dockerfile, remove pending db migrations from code

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -73,18 +73,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-/*
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
 
-    var context = services.GetRequiredService<MyDbContext>();
-    if (context.Database.GetPendingMigrations().Any())
-    {
-        context.Database.Migrate();
-    }
-}
-*/
+// using (var scope = app.Services.CreateScope())
+// {
+//     var services = scope.ServiceProvider;
+
+//     var context = services.GetRequiredService<MyDbContext>();
+//     if (context.Database.GetPendingMigrations().Any())
+//     {
+//         context.Database.Migrate();
+//     }
+// }
+
 
 app.Run();
 
